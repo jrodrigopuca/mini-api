d93912de101daeee905be90694053499897b92b1
jarp <r@geeks.ms>
2020-09-27 17:45:04 -0300

    documentacion

--- global.php
+++ global.php
@@ -9,12 +9,16 @@ function traerProducto($id){
     return enviarRespuesta(1,$listado[$id]);
 }
 
+function agregarProducto($array){
+    #var_dump($array);
+    return enviarRespuesta(1,"Agregado correctamente");
+}
+
 function enviarRespuesta($resultado, $data){
     $respuesta=array(
         "resultado"=>$resultado==1?"Ok":"Error", 
         "data"=>$data);
     return json_encode($respuesta);
 }
-
 
 ?>
--- index-sw.php
+++ index-sw.php
@@ -3,14 +3,30 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once 'global.php';
 use Steampixel\Route;
 
+/**
+ * @OA\Info(title="Mini API", version="0.1")
+ */
+
 Route::add('/', function() {
     echo json_encode(["nombre"=>'Bienvenido a index']);
 });
 
+/**
+ * @OA\Get(
+ *     path="/api",
+ *     @OA\Response(response="200", description="intro al api")
+ * )
+ */
 Route::add('/api', function() {
     echo json_encode(["nombre"=>'Bienvenido al API']);
 });
 
+/**
+ * @OA\Get(
+ *     path="/api/productos",
+ *     @OA\Response(response="200", description="traer productos")
+ * )
+ */
 Route::add('/api/productos', function() {
     echo traerProductos();
 }, 'get');
@@ -19,8 +35,8 @@ Route::add('/api/productos/([0-9]*)', function($id) {
     echo traerProducto($id);
 }, 'get');
 
-Route::add('/api/productos', function() {
-    echo agregarProducto(json_decode(file_get_contents('php://input')));
+Route::add('/api/productos/editar', function() {
+    echo json_encode($_POST);
 }, 'post');
 
 
